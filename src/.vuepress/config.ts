f78247598022ef8ba08222d7ee9c8dffb043edc3
Timm Ortloff <me@tortloff.de>
2021-09-11 02:07:33 +0200

    Changed config.js to config.ts and migrated to vuepress2

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,71 @@
+import { defineUserConfig } from "@vuepress/cli";
+import { DefaultThemeOptions } from "@vuepress/theme-default"
+import { navbar, sidebar } from "./configs";
+
+const { description } = require('../../package')
+
+export default defineUserConfig<DefaultThemeOptions>({
+
+  base: '/',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'SALS Documentation & Help',
+      description: ''
+    },
+    '/de/': {
+      lang: 'de-DE',
+      title: 'SALS Dokumentation & Hilfe',
+      description: ''
+    }
+  },
+
+  themeConfig: {
+
+    locales: {
+      '/': {
+        selectLanguageText: 'Languages',
+        selectLanguageName: 'English',
+        editLinkText: 'Edit this page',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        navbar: navbar.en,
+        sidebar: sidebar.en
+      },
+      '/de/': {
+        selectLanguageText: 'Languages',
+        selectLanguageName: 'Deutsch',
+        editLinkText: 'Diese Seite bearbeiten',
+        serviceWorker: {
+          updatePopup: {
+            message: "Eine neue Version ist verfügbar",
+            buttonText: "Neu laden"
+          }
+        },
+        navbar: navbar.de,
+        sidebar: sidebar.de
+      }
+    },
+
+    repo: 'https://github.com/SALS-App/Docs',
+    editLinks: true,
+    docsDir: 'src/',
+    editLinkText: '',
+    contributors: false,
+    lastUpdated: true,
+    nav: [],
+    sidebar: {}
+  },
+  plugins: []
+});
